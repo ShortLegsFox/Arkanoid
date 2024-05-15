cb2b585b6f56fa87b7f99b302137da993bfda635
ShortLegsFox <ian.bellot001@gmail.com>
2024-05-15 12:05:36 +0200

    Animation du bonus lors de la chute

--- game-objects/bonus.c
+++ game-objects/bonus.c
@@ -3,6 +3,7 @@
 
 struct Bonus stats_bonus;
 bool bonus_en_cours;
+int timer = 0;
 
 void Initialise_Bonus(int coord_x, int coord_y) {
     stats_bonus.pos_x = coord_x;
@@ -18,5 +19,19 @@ void Tomber_Bonus() {
         SDL_Rect bonus = {stats_bonus.pos_x, stats_bonus.pos_y, source_texture_brique_bonus_s.w,
                           source_texture_brique_bonus_s.h};
         SDL_BlitSurface(textures_objets, &source_texture_brique_bonus_s, surface_fenetre, &bonus);
+
+        if(timer % 5 == 0) {
+            if (source_texture_brique_bonus_s.x == 480) {
+                source_texture_brique_bonus_s.x = 256;
+            } else {
+                source_texture_brique_bonus_s.x += 32;
+            }
+        }
+
+        if(timer > 100) {
+            timer = 0;
+        }
+
+        timer++;
     }
 }
